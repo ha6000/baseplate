5660701022c6870fd67ac00ebe26d98ba6ae2cd2
Harry <krugerh27@gmail.com>
2020-08-06 20:19:29 +0200

    Removing ajv from command.js

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -1,14 +1,5 @@
-const Ajv = require('ajv');
-const ajv = new Ajv();
 const { readdirSync } = require('fs');
 const { join } = require('path');
-
-const validateCommand = ajv.compile({
-	name: { type: String },
-	aliases: { type: Array, items: String },
-	run: { type: Function},
-	required: ['name', 'run']
-});
 
 const commandFolder = join(__dirname, '../commands');
 
@@ -22,8 +13,6 @@ module.exports = (client) => {
 			commandFile = Object.assign({}, {
 				aliases: []
 			}, commandFile);
-			const commandValid = validateCommand(commandFile);
-			if (!commandValid) return console.error(`Command '${file}' invalid: `, ajv.errorsText(validateCommand.errors));
 
 			client.commands.set(commandFile.name, commandFile);
 			commandFile.aliases.forEach(alias => client.aliases.set(alias, commandFile.name));
